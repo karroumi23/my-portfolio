06922874f96d666098ef4fd2e98657cb13beba92
karroumi <anasskarroumi2016@gmail.com>
2024-09-25 13:25:56 +0200

    create function slide-bar(hidding & showing)

---
+++ main.js
@@ -0,0 +1,7 @@
+//------create function slide-bar(hidding & showing)
+let menuIcon = document.getElementById("menu-icon");
+let slideBar = document.getElementById("slide-bar");
+function showMenu() {
+  menuIcon.classList.toggle("fa-times");
+  slideBar.classList.toggle("active");
+}
