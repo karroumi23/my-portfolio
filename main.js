4b0d93a6ec727fb48397faaa8bdccb27f837f834
karroumi <anasskarroumi2016@gmail.com>
2024-09-25 13:40:30 +0200

    hide the sidebar when clicking anywhere outside of it

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ function showMenu() {
   menuIcon.classList.toggle("fa-times");
   slideBar.classList.toggle("active");
 }
+//<hide the sidebar when clicking anywhere outside of it>
+document.addEventListener("click", function (event) {
+    if (!slideBar.contains(event.target) && !menuIcon.contains(event.target)) {
+      if (slideBar.classList.contains("active")) {
+        slideBar.classList.remove("active");
+        menuIcon.classList.remove("fa-times");
+      }
+    }
+  });
