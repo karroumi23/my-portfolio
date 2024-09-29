59afbb50526e2346870537d103299cf375933f7f
karroumi <anasskarroumi2016@gmail.com>
2024-09-29 20:25:59 +0200

    create a function to add background color to the header when i scroll down

--- main.js
+++ main.js
@@ -14,3 +14,9 @@ document.addEventListener("click", function (event) {
       }
     }
   });
+  // create a function to add background color to the header when i scroll down
+    const header = document.querySelector("header");
+
+    window.addEventListener ("scroll", function(){
+      header.classList.toggle("sticky", window.scrollY > 120);
+    })
