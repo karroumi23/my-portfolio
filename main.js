fb7d2b7de7664de5b212549125fc176b11ec7a5c
karroumi <anasskarroumi2016@gmail.com>
2024-10-02 10:13:59 +0200

    add button descreption and paragraph to the bottom of cards and style it with css

--- main.js
+++ main.js
@@ -7,16 +7,23 @@ function showMenu() {
 }
 //<hide the sidebar when clicking anywhere outside of it>
 document.addEventListener("click", function (event) {
-    if (!slideBar.contains(event.target) && !menuIcon.contains(event.target)) {
-      if (slideBar.classList.contains("active")) {
-        slideBar.classList.remove("active");
-        menuIcon.classList.remove("fa-times");
-      }
+  if (!slideBar.contains(event.target) && !menuIcon.contains(event.target)) {
+    if (slideBar.classList.contains("active")) {
+      slideBar.classList.remove("active");
+      menuIcon.classList.remove("fa-times");
     }
-  });
-  // create a function to add background color to the header when i scroll down
-    const header = document.querySelector("header");
+  }
+});
+// create a function to add background color to the header when i scroll down
+const header = document.querySelector("header");
 
-    window.addEventListener ("scroll", function(){
-      header.classList.toggle("sticky", window.scrollY > 120);
-    })
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 120);
+});
+
+// create function to display and hiding description paragraph
+
+// let descriptionBtn = document.getElementById("descriptionBtn");
+// let descriptionParagraph = document.getElementById("descriptionParagraph");
+
+
